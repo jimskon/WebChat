0727b8f2f1f2adb292f3bebbc82d3e7767480e61
Jim Skon <skonjp@kenyon.edu>
2017-02-25 11:07:07 -0500

    Add fix for zero length string

--- selfChat.js
+++ selfChat.js
@@ -6,12 +6,15 @@ function pageInit() {
 
 // Respond to send button
 function addText() {
-	// Get the text from the text box
-	var inText = document.chatForm.textInput.value;
+    // Get the text from the text box
+    var inText = document.chatForm.textInput.value;
+    
+    // Make sure something is there
+    if (inText.length < 1) return;
+    
+    // Clear the input text
+    document.chatForm.textInput.value = "";
 	
-	// Clear the input text
-	document.chatForm.textInput.value = "";
-	
-	// add to the bottom of the chat box
-	document.chatForm.chatText.innerHTML += "You: " + inText+"\n";
+    // add to the bottom of the chat box
+    document.chatForm.chatText.innerHTML += "You: " + inText+"\n";
 }
